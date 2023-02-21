f41a4ce23965866b9aa04093c0bcbac49fefd4f5
Giancarlo Pernudi Segura <giancarlopernudi@hotmail.com>
2023-02-21 10:09:54 -0700

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use clap::Parser;
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    filename: Option<String>,
+}
 
 fn main() -> std::io::Result<()> {
+    let args = Args::parse();
     let code: Vec<char> =
-    if env::args().len() == 1 {
-        println!("Reading from stdin");
-        Vec::new()
-    } else {
-        let filename = env::args().nth(1).unwrap();
+    if args.filename.is_some() {
+        let filename = args.filename.unwrap();
         println!("Reading from file: {}", &filename);
         let mut file = File::open(&filename)?;
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
         contents.chars().collect()
+    } else {
+        println!("Reading from stdin");
+        Vec::new()
     };
 
     assert_eq!(code.iter().filter(|&c| *c == '😒').count(), code.iter().filter(|&c| *c == '😡').count());
